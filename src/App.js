de079587f2916850d5d73335e9d96ef9cef3022d
Harleigh <155698437+Harweeeee@users.noreply.github.com>
2024-04-03 10:29:44 +1000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function App() {
+    return (
+        <div className="App">
+            <h1>Soltice Sky</h1>
+            <footer>
+                This app was created by <a href="#">Harleigh Gallant</a>and
+                open-sourced on <a href="#">Github</a>
+            </footer>
+        </div>
+    )
+}
